4b4ffff1619268d39a9c0ad5ec2da5c8110685e9
Aman Ahamed <amanahamed04@gmail.com>
2025-12-14 13:57:23 +0530

    frontend - basic setup + shadcn ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-red-700">
+      Hello world
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
